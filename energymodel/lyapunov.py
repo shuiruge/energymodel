24b6f7ed62e2a589a2a8b884db9fe532b74a10e0
shuiruge <shuiruge@hotmail.com>
2022-09-15 23:24:02 +0800

    Fix Lyapunov, using non-persistant approach.

--- energymodel/lyapunov.py
+++ energymodel/lyapunov.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 from typing import Callable
 
-from .models import get_adaptive_temperature, check_nan
+from .models import get_adaptive_temperature
 from .sde import SDESolver, SDE
-from .utils import TensorLike, nest_map
+from .utils import TensorLike, nest_map, check_nan
 
 
 @nest_map
@@ -28,22 +28,24 @@ class Lyapunov:
   def __init__(
       self,
       vector_field: Callable[[TensorLike], TensorLike],
-      initial: TensorLike,
+      resample: Callable[[int], TensorLike],
       solver: SDESolver,
       t: float,
       T: float = None,
+      batch_size: int = 128,
   ):
     self.vector_field = vector_field
-    self.initial = to_tensor(initial)
+    self.resample = resample
     self.solver = solver
     self.t = to_tensor(t)
     if T is None:
       self.T = get_adaptive_temperature(
-          vector_field_samples=vector_field(initial),
+          vector_field_samples=vector_field(resample(batch_size)),
           t=self.t,
       )
     else:
       self.T = to_tensor(T)
+    self.batch_size = batch_size
 
     @nest_map
     def cholesky(s):
@@ -53,10 +55,8 @@ class Lyapunov:
         vector_field=lambda x, t: vector_field(x),
         cholesky=lambda x, t, s: cholesky(s),
     )
-    self.particles = initialize_variable(self.initial, trainable=False)
 
   def __call__(self):
-    new_particles = self.solver(self.sde, 0., self.t, self.particles)
-    assign_variable(self.particles, new_particles)
-    check_nan(self.particles, 'particles')
-    return self.particles
+    particles = self.solver(self.sde, 0., self.t, self.resample(self.batch_size))
+    check_nan(particles, 'particles')
+    return particles
