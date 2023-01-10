3eebb08cbfed58626ecae51417179c4c8102e29f
shuiruge <shuiruge@whu.edu.cn>
2023-01-10 09:06:07 +0800

    Change Python version.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 # shell.nix
 with import <nixpkgs> { };
 let
-  pythonPackages = python37Packages;
+  pythonPackages = python39Packages;
 in pkgs.mkShell rec {
   name = "energymodelEnv";
   venvDir = "./.venv";
@@ -20,9 +20,9 @@ in pkgs.mkShell rec {
   postShellHook = ''
     # Setup TensorFlow environment.
     export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.cudaPackages_10_1.cudatoolkit}/lib:${pkgs.cudaPackages_10_1.cudatoolkit}/lib:${pkgs.cudaPackages_10_1.cudatoolkit.lib}/lib:$LD_LIBRARY_PATH
-    alias pip="PIP_PREFIX='$(pwd)/_build/pip_packages' TMPDIR='$HOME' \pip"
-    export PYTHONPATH="$(pwd)/_build/pip_packages/lib/python3.7/site-packages:$PYTHONPATH"
-    export PATH="$(pwd)/_build/pip_packages/bin:$PATH"
+    alias pip="PIP_PREFIX='$(pwd)/.build/pip_packages' TMPDIR='$HOME' \pip"
+    export PYTHONPATH="$(pwd)/.build/pip_packages/lib/python3.9/site-packages:$PYTHONPATH"
+    export PATH="$(pwd)/.build/pip_packages/bin:$PATH"
     unset SOURCE_DATE_EPOCH
     # Setup local virtual environment.
     pip install . -r requirements.txt -i https://pypi.douban.com/simple
